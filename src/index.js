4abe0354871b0c8962f53bf93c3490a9504ef61c
annalikhuta <annalikhuta@gmail.com>
2019-03-15 14:38:25 +0300

    is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+   let objSimpleBase={};
+   let maxPowerCount={};
+   let min=0;
+ 
+
+function makeSimpleArr(base){
+  let arr=[];
+  [base].forEach(item => {
+    for(let j=2; j<=item; j++){
+      while(item%j==0){
+        arr.push(j);
+        item=item/j;
+      }
+    }
+  });
+
+  for(let i =0; i<=arr.length-1; i++){
+    objSimpleBase[arr[i]]=0;
+    maxPowerCount[arr[i]]=0;
+  }
+  for(let i =0; i<=arr.length-1; i++){
+    objSimpleBase[arr[i]]+=1;
+
+  }
+  
+let arrOfPow=[];
+  for (let key in maxPowerCount ){
+    maxPowerCount[key]= Math.floor(maxPowerForThisNumber(key, number)/objSimpleBase[key])
+    arrOfPow.push(maxPowerCount[key])
+  }
+
+  min=Math.min.apply(null, arrOfPow)
+
+
 }
+function maxPowerForThisNumber(param, number){
+  let count=0;
+  let powParam=1;
+  while(true){
+    powParam*=param;
+    let add=Math.floor(number/powParam);
+    if(add==0){
+      return Math.floor(count);
+
+    }
+    count+=add;
+  }
+}
+
+makeSimpleArr(base);
+// console.log(min)
+return min
+
+
+ }
+
+// getZerosCount(4821527, 250)
+// getZerosCount(18835759, 190)
